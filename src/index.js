2f66034b8c2ed3da90773f510c3ca4282561b74d
lifesign <leocloudy@gmail.com>
2020-02-27 21:37:13 +0800

    feat: allow loading `.spaceship.json` config file

--- src/index.js
+++ src/index.js
@@ -8,26 +8,72 @@
  */
 
 const _ = require("lodash")
-const net = require("net")
-
-function updateData() {
-
-    //miniWindow    
-  here.setMiniWindow({ title: "Hello MiniWindow", detail: "This is a demo here plugin" })
-
-
-
-}
+const spaceshipConfName = '.spaceship.json'
+const homeDir = '/Users/$(whoami)'
 
 here.onLoad(() => {
-  updateData()
-  // Update every 2 hours
-  setInterval(updateData, 2*3600*1000);
+  ensureConfigExists()
+  setupSpaceship()
 })
 
-net.onChange((type) => {
-  console.log("Connection type changed:", type)
-  if (net.isReachable()) {
-    updateData()
-  }
-})
+function ensureConfigExists() {
+  here.exec(`
+[ ! -f ${homeDir}/${spaceshipConfName} ] && cp ./template/spaceship.json ${homeDir}/${spaceshipConfName} && echo "config init done."
+`)
+  .then((output) => {
+    console.log(output)
+  }).catch((err) => {console.log(`config init error: ${err}`)})
+}
+
+function setupSpaceship() {
+  console.log('setup begin ........')
+  //cat ~/.spaceship.json
+  here.exec(`
+cat ${homeDir}/${spaceshipConfName}
+`).then((output) => {
+    const config = JSON.parse(output);
+  // console.log(JSON.stringify(config.cabins))
+    const popOvers = _.map(config.cabins, (cabin) => {
+      return {
+        title: cabin.name,
+        onClick: () => {
+          console.log(cabin.cmd)
+          here.exec(`osascript -e '
+tell application "Terminal"
+  reopen
+  activate
+  do script "${cabin.cmd}" in front window
+end tell
+'`)
+          .then((output) => {
+            console.log(`cabin execute cmd output: ${output}`)
+          })
+          .catch((err) => {console.log(`cabin execute cmd error: ${output}`)})
+        }
+      }
+    })
+
+    here.setMiniWindow({
+        title: `Spaceship Loading..`,
+        detail: "Click to Edit Config",
+        onClick: () => {
+          //quick open .spaceship.json
+          here.exec(`
+open ${homeDir}/${spaceshipConfName}
+`)
+          .then((output) => {
+            console.log(`open config file: output: ${output}`)
+            here.systemNotification(`温馨提醒`, `变更配置后请重启 Here 或者重载 Spaceship 插件生效`)
+          }).catch((err) => {console.log(`open config file error: ${err}`)})
+        },
+        popOvers: popOvers
+    })
+
+    console.log('mini window setup ready...')
+
+  })
+  .catch((err) => {
+    // TODO check json syntax
+    console.error(err)
+  })
+}
